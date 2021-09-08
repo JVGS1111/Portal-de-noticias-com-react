068afeb29efb97426838aab0a54051aa494a45fa
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-09-08 14:36:00 -0300

    implementada pagina Sobre e bug de css do Header corrigido

--- portal-noticias/src/paginas/Sobre.jsx
+++ portal-noticias/src/paginas/Sobre.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import '../assets/css/paginas/Sobre.css'
 function Sobre() {
     return (
         <main className="main-sobre">
             <div className="container">
                 <h2 className="titulo-sobre">Sobre</h2>
                 <div className="sobre-texto-wrapper">
-                    <p className="sobre-texto">Este é um projeto pequeno que criei para apender React.
-                        Este é um pequeno portal de noticias, eu criei com a intencao de aprender mais sobre componentes
-                        funcionais, rotas e hooks
+                    <p className="sobre-texto">Este é um pequeno portal de noticias feito com React,
+                        criei este projeto para aprender React, neste projeto apliquei componentes
+                        funcionais, rotas e hooks.
                     </p>
                 </div>
             </div>
